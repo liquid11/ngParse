95f58f3c26e4a0afa7f20a06c5429dbd85ca375e
liquid11 <orangevolution@yahoo.com>
2015-09-08 20:21:07 +0500

    cmmmented the review need to add the parse rest

---
+++ assets/app.js
@@ -0,0 +1,99 @@
+/**
+ * Created by tezrosolutions on 08/09/2015.
+ */
+/**
+ * Created by tezrosolutions on 08/09/2015.
+ */
+/**
+ * Created by tezrosolutions on 12/08/2015.
+ */
+
+var storeApp = angular.module('storeApp', []);
+
+storeApp.controller('StoreController', function () {
+    this.products = gems;
+});
+
+//storeApp.controller('PanelController',function() {
+//   /*   set initialization values   */
+//   this.tab =1;
+//   /* tab selection function */
+//  this.selectTab= function(setTab){
+//     this.tab = setTab;
+//   };
+//  this.isSelected = function(chk){
+//     return this.tab === chk;
+// };
+//});
+
+
+//the dash in html translates to camelCasing in javascript
+/*the directive*/
+
+
+/* gems json object */
+/*var gems = [
+ {
+ name:'Azurite' ,
+ price:'22.49' ,
+ description:'2,49' ,
+ canPurchase:true,
+ soldOut:true ,
+ reviews:[
+ {
+ stars:'3',
+ body:'.... ',
+ author:'tim@jones.com'} ,
+ {
+ stars:'4',
+ body:'not so good ',
+ author:'james@webb.com'} ,
+ {
+ stars:'1',
+ body:'this ....',
+ author:'hater@gonnaHate.com'}
+ ]
+ },
+ {
+ name:'ruby' ,
+ price:'5.49' ,
+ description:'.....' ,
+ canPurchase:true ,
+ soldOut:false ,
+ reviews:[
+ {
+ stars:'5',
+ body:'this is awesome ',
+ author:'tim@jones.com'} ,
+ {
+ stars:'5',
+ body:'so good ',
+ author:'james@webb.com'} ,
+ {
+ stars:'1',
+ body:'this ....',
+ author:'hater@gonnaHate.com'}
+ ]
+ },
+ {
+ name:'zerkon' ,
+ price:'35' ,
+ description:'....' ,
+ canPurchase:true ,
+ soldOut:false
+ }/!*,
+ reviews:[
+ {
+ stars:'4',
+ body:'this is awesome ',
+ author:'tim@jones.com'} ,
+ {
+ stars:'2',
+ body:'so good ',
+ author:'james@webb.com'} ,
+ {
+ stars:'1',
+ body:'this ....',
+ author:'hater@gonnaHate.com'}
+ ]*!/
+ ];*/
